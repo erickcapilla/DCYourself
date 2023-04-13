630c6284f92f2dc6d9c3fff834478e9dc830c7aa
Erick Capilla <erickpoblano25@gmail.com>
2023-04-12 22:23:13 -0600

    Added more screens

---
+++ app/src/main/java/com/erickcapilla/dcyourself/BirthDate.kt
@@ -0,0 +1,11 @@
+package com.erickcapilla.dcyourself
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BirthDate : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_birth_date)
+    }
+}
---
+++ app/src/main/java/com/erickcapilla/dcyourself/Graph.kt
@@ -0,0 +1,11 @@
+package com.erickcapilla.dcyourself
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Graph : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_graph)
+    }
+}
